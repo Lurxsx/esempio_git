b1450737675ca9437d263d270a66e901d1d8cd8c
Lurxsx <lorex.spt@gmail.com>
2024-01-12 13:24:27 +0100

    aggiunto for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bella li 4ez");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("yo");
+        }
     }
 }
